faf6515771235b1aff962deeea7a12d4345e25c7
Meltem <melltemkilic@gmail.com>
2017-05-11 16:51:40 +0000

    new employees added two first and second store

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -4,8 +4,18 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
+require 'pry'
 
 puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+binding.pry
+  @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+  @store1.employees.create(first_name: "Rafael", last_name: "Nadal", hourly_rate: 60)
+
+  @store2.employees.create(first_name: "Roger", last_name: "Federer", hourly_rate: 60)
+  @store2.employees.create(first_name: "Andy", last_name: "Roddick", hourly_rate: 60)
