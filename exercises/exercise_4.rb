018441c169cab1c30fa54afb3c46536eb33c399b
Meltem <melltemkilic@gmail.com>
2017-05-11 16:09:29 +0000

    a subset of stores loaded to the console based on some criterias

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,27 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+  @store3 = Store.new(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+  @store4 = Store.new(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+  @store5 = Store.new(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+  @store3.save
+  @store4.save
+  @store5.save
+
+  puts Store.count
+
+  @mens_stores = Store.where(mens_apparel: true)
+  puts @mens_stores
+
+  @mens_stores.each do |item|
+    puts item.name
+    puts item.annual_revenue
+  end
+
+@women_stores = Store.where(womens_apparel: true, annual_revenue: 0..1000000)
+
+puts @women_stores.count
+
+
